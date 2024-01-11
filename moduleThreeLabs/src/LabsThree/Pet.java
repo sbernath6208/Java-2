1c93dd0f02422d7387cc9759498a5f626e4e7983
sbernath6208 <sabernath@student.rockvalleycollege.edu>
2024-01-11 10:44:04 -0600

    Add files via upload

---
+++ moduleThreeLabs/src/LabsThree/Pet.java
@@ -0,0 +1,60 @@
+// Stuart Bernath	uin: 669791508 
+// 2/19/23 
+// 23SP-CSC275A-BL-13540 
+// 
+  public class Pet 
+  {
+    private String name; 
+    private int age; 
+    private String color; 
+
+    public Pet() 
+    {
+      this.name = ""; 
+      this.age = 0; 
+      this.color = ""; 
+    }
+
+    public Pet(String userName, int userAge, String userColor) 
+    {
+      this.name = userName; 
+      this.age = userAge; 
+      this.color = userColor; 
+    }
+
+    public String getName() 
+    {
+      return this.name; 
+    }
+
+    public void setName(String userName) 
+    {
+      this.name = userName; 
+    }
+
+    public int getAge() 
+    {
+      return this.age; 
+    }
+
+    public void setAge(int userAge) 
+    {
+      this.age = userAge; 
+    }
+
+    public String getColor() 
+    {
+      return this.color; 
+    }
+
+    public void setColor(String userColor) 
+    {
+      this.color = userColor; 
+    }
+
+    public String toString() 
+    {
+      return "Name: " + this.name + ", Age: " + this.age + ", Color: " + this.color + "\n"; 
+    }
+
+  }
